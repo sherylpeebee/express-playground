8d5da9722b77cacd3599acfdd019f73e029b5bda
AgileLabs <samer@agilelabs.com>
2015-06-30 09:53:16 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/data/:id', function(req, res) {
+  var data = require("../data.json");
+  res.send(data.quotes[+req.params.id - 1]);
+});
+
+router.get('/data', function(req, res) {
+  res.send("Got it!\n");
+});
+
+router.post('/data', function(req, res) {
+  res.send("Posted!\n");
 });
 
 module.exports = router;
