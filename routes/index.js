311edb9f73452e61fd3c5184a2d7f309431567aa
Sheryl Boughton <sherpbo@gmail.com>
2015-06-30 11:36:43 -0700

    a basic express API with JADE

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var path = require("path");
+// req.query
 
 router.get('/data/:id', function(req, res) {
   var data = require("../data.json");
@@ -12,6 +15,26 @@ router.get('/data', function(req, res) {
 
 router.post('/data', function(req, res) {
   res.send("Posted!\n");
+});
+router.get('/quotes', function(req, res) {
+  var data = require("../data.json");
+  var quotes = data.quotes;
+  res.render("quotes", {quotes: quotes});
+});
+router.post('/quotes', function(req, res) {
+  var newQuote = req.body.SUBMISSION ;
+  var data = require("../data.json");
+  data.quotes.push(newQuote);
+  fs.writeFile(path.join(__dirname, "data.json"), JSON.stringify(data), function(err){
+    //This allows us to use absolute paths and join together like strings ^^!!!
+    //see documentation on nodemodule "path"
+    if (err){
+      console.error(err);
+      res.status(500).send("We could not save the quote, please try again later");
+    }
+  res.redirect("/quotes");
+  });
+  // res.send("done.");
 });
 
 module.exports = router;
